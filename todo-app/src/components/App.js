482b83eec91262fc0517e599e4af594f0f175b6b
tekihei2317 <tekihei2317@gmail.com>
2022-01-13 16:44:34 +0900

    refactor: split into components(todo list and todo item)

--- todo-app/src/components/App.js
+++ todo-app/src/components/App.js
@@ -1,6 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { fetchTodos } from "../services/fetch-todos";
 
+const TodoTitle = ({ title, as }) => {
+  if (as === "h1") return <h1>{title}</h1>;
+  if (as === "h2") return <h2>{title}</h2>;
+  return <p>{title}</p>;
+};
+
+const TodoItem = ({ todo }) => {
+  return (
+    <li>
+      {todo.content}
+      <button>{todo.done ? "未完了リストへ" : "完了リストへ"}</button>
+      <button>削除</button>
+    </li>
+  );
+};
+
+const TodoList = ({ todoList }) => {
+  return (
+    <ul>
+      {todoList.map((todo) => (
+        <TodoItem todo={todo} key={todo.id} />
+      ))}
+    </ul>
+  );
+};
+
 function App() {
   const [todoList, setTodoList] = useState([]);
 
@@ -18,31 +44,16 @@ function App() {
   return (
     <>
       <h1>TODO進捗管理</h1>
+      <TodoTitle title="TODOタイトル管理" as="h1" />
 
       <textarea />
       <button>+ TODOを追加</button>
 
-      <h2>未完了TODOリスト</h2>
-      <ul>
-        {incompletedList.map((todo) => (
-          <li key={todo.id}>
-            {todo.content}
-            <button>{todo.done ? "未完了リストへ" : "完了リストへ"}</button>
-            <button>削除</button>
-          </li>
-        ))}
-      </ul>
+      <TodoTitle title="未完了TODOリスト" as="h2" />
+      <TodoList todoList={incompletedList} />
 
-      <h2>完了TODOリスト</h2>
-      <ul>
-        {completedList.map((todo) => (
-          <li key={todo.id}>
-            {todo.content}
-            <button>{todo.done ? "未完了リストへ" : "完了リストへ"}</button>
-            <button>削除</button>
-          </li>
-        ))}
-      </ul>
+      <TodoTitle title="完了TODOリスト" as="h2" />
+      <TodoList todoList={completedList} />
     </>
   );
 }
