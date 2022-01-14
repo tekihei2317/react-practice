1e5b3d48119032ade106db9d9281f3c53398a85b
tekihei2317 <tekihei2317@gmail.com>
2022-01-14 18:13:33 +0900

    feat: タスクのステータスの更新機能と削除機能を実装

--- todo-app/src/components/App.jsx
+++ todo-app/src/components/App.jsx
@@ -7,28 +7,45 @@ const TodoTitle = ({ title, as }) => {
   return <p>{title}</p>;
 };
 
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo, toggleTodoListItemStatus, deleteTodoListItem }) => {
+  const handleToggleTodoListItemStatus = () => toggleTodoListItemStatus(todo.id, todo.done);
+  const handleDeleteTodoListItem = () => deleteTodoListItem(todo.id);
+
   return (
     <li>
       {todo.content}
-      <button>{todo.done ? "未完了リストへ" : "完了リストへ"}</button>
-      <button>削除</button>
+      <button onClick={handleToggleTodoListItemStatus}>{todo.done ? "未完了リストへ" : "完了リストへ"}</button>
+      <button onClick={handleDeleteTodoListItem}>削除</button>
     </li>
   );
 };
 
-const TodoList = ({ todoList }) => {
+const TodoList = ({ todoList, toggleTodoListItemStatus, deleteTodoListItem }) => {
   return (
     <ul>
       {todoList.map((todo) => (
-        <TodoItem todo={todo} key={todo.id} />
+        <TodoItem
+          todo={todo}
+          key={todo.id}
+          toggleTodoListItemStatus={toggleTodoListItemStatus}
+          deleteTodoListItem={deleteTodoListItem}
+        />
       ))}
     </ul>
   );
 };
 
+const TodoAdd = ({ inputElement, handleAddTodoListItem }) => {
+  return (
+    <>
+      <textarea ref={inputElement} />
+      <button onClick={handleAddTodoListItem}>+ TODOを追加</button>
+    </>
+  );
+};
+
 function App() {
-  const { todoList, addTodoListItem } = useTodo();
+  const { todoList, addTodoListItem, toggleTodoListItemStatus, deleteTodoListItem } = useTodo();
   const incompletedList = todoList.filter((todo) => !todo.done);
   const completedList = todoList.filter((todo) => todo.done);
 
@@ -47,14 +64,21 @@ function App() {
       <h1>TODO進捗管理</h1>
       <TodoTitle title="TODOタイトル管理" as="h1" />
 
-      <textarea ref={inputElement} />
-      <button onClick={handleAddTodoListItem}>+ TODOを追加</button>
+      <TodoAdd inputElement={inputElement} handleAddTodoListItem={handleAddTodoListItem} />
 
       <TodoTitle title="未完了TODOリスト" as="h2" />
-      <TodoList todoList={incompletedList} />
+      <TodoList
+        todoList={incompletedList}
+        toggleTodoListItemStatus={toggleTodoListItemStatus}
+        deleteTodoListItem={deleteTodoListItem}
+      />
 
       <TodoTitle title="完了TODOリスト" as="h2" />
-      <TodoList todoList={completedList} />
+      <TodoList
+        todoList={completedList}
+        toggleTodoListItemStatus={toggleTodoListItemStatus}
+        deleteTodoListItem={deleteTodoListItem}
+      />
     </>
   );
 }
